4dc1f6c621076c2d9bc51ef33ae92ebf59e3ca06
Alfonso Sánchez <alfonso1799@gmail.com>
2025-11-02 17:25:14 +0100

    - Player movement is working, we'll have to see if it's working ok, there's "a lot" of maths involved.
    -norminette to handle_keys

--- src/events/handle_keys.c
+++ src/events/handle_keys.c
@@ -13,52 +13,52 @@
 
 void	key_press(t_game *game, int key)
 {
-    if (key == MLX_KEY_ESCAPE)
-        game->keys.esc = TRUE;
-    if (key == MLX_KEY_W)
-        game->keys.w = TRUE;
-    if (key == MLX_KEY_S)
-        game->keys.s = TRUE;
-    if (key == MLX_KEY_A)
-        game->keys.a = TRUE;
-    if (key == MLX_KEY_D)
-        game->keys.d = TRUE;
-    if (key == MLX_KEY_LEFT)
-        game->keys.left = TRUE;
-    if (key == MLX_KEY_RIGHT)
-        game->keys.right = TRUE;
+	if (key == MLX_KEY_ESCAPE)
+		game->keys.esc = TRUE;
+	if (key == MLX_KEY_W)
+		game->keys.w = TRUE;
+	if (key == MLX_KEY_S)
+		game->keys.s = TRUE;
+	if (key == MLX_KEY_A)
+		game->keys.a = TRUE;
+	if (key == MLX_KEY_D)
+		game->keys.d = TRUE;
+	if (key == MLX_KEY_LEFT)
+		game->keys.left = TRUE;
+	if (key == MLX_KEY_RIGHT)
+		game->keys.right = TRUE;
 }
 
 void	key_release(t_game *game, int key)
 {
-    if (key == MLX_KEY_W)
-        game->keys.w = FALSE;
-    if (key == MLX_KEY_S)
-        game->keys.s = FALSE;
-    if (key == MLX_KEY_A)
-        game->keys.a = FALSE;
-    if (key == MLX_KEY_D)
-        game->keys.d = FALSE;
-    if (key == MLX_KEY_LEFT)
-        game->keys.left = FALSE;
-    if (key == MLX_KEY_RIGHT)
-        game->keys.right = FALSE;
+	if (key == MLX_KEY_W)
+		game->keys.w = FALSE;
+	if (key == MLX_KEY_S)
+		game->keys.s = FALSE;
+	if (key == MLX_KEY_A)
+		game->keys.a = FALSE;
+	if (key == MLX_KEY_D)
+		game->keys.d = FALSE;
+	if (key == MLX_KEY_LEFT)
+		game->keys.left = FALSE;
+	if (key == MLX_KEY_RIGHT)
+		game->keys.right = FALSE;
 }
 
 void	key_event(mlx_key_data_t keydata, void *param)
 {
-    t_game	*game;
+	t_game	*game;
 
-    game = (t_game *)param;
-    if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
-    {
-        key_press(game, keydata.key);
-        return ;
-    }
-    if (keydata.action == MLX_RELEASE)
-    {
-        key_release(game, keydata.key);
-    }
+	game = (t_game *)param;
+	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
+	{
+		key_press(game, keydata.key);
+		return ;
+	}
+	if (keydata.action == MLX_RELEASE)
+	{
+		key_release(game, keydata.key);
+	}
 }
 
 int	close_game(t_game *game)
@@ -68,15 +68,5 @@ int	close_game(t_game *game)
 		mlx_close_window(game->mlx);
 		return (0);
 	}
-	// if (game->wall)
-	// 	mlx_destroy_image(game->mlx, game->wall);
-	// if (game->player)
-	// 	mlx_destroy_image(game->mlx, game->player);
-	// if (game->collectible)
-	// 	mlx_destroy_image(game->mlx, game->collectible);
-	// if (game->exit)
-	// 	mlx_destroy_image(game->mlx, game->exit);
-	// mlx_destroy_window(game->mlx, game->win);
-	// exit(0);
 	return (0);
 }
--- src/events/player_movement.c
+++ src/events/player_movement.c
@@ -3,58 +3,95 @@
 /*                                                        :::      ::::::::   */
 /*   player_movement.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alfsanch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rzamolo- <rzamolo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 19:03:23 by alfsanch          #+#    #+#             */
-/*   Updated: 2024/07/17 17:05:05 by alfsanch         ###   ########.fr       */
+/*   Updated: 2025/10/30 16:43:11 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "cub3d.h"
 
-static void	update_player_position(t_game *game, int new_x, int new_y)
+#include "../../include/cub3d.h"
+
+t_bool	is_wall(t_game *game, int x, int y)
 {
-	if (game->on_exit)
-		game->map[game->player_y][game->player_x] = 'E';
-	else
-		game->map[game->player_y][game->player_x] = '0';
-	game->on_exit = (game->map[new_y][new_x] == 'E');
-	game->player_x = new_x;
-	game->player_y = new_y;
-	game->map[new_y][new_x] = 'P';
+	if (x < 0 || x >= game->map.width || y < 0 || y >= game->map.height)
+		return (TRUE);
+	if (game->map.grid[y][x] == '1')
+		return (TRUE);
+	return (FALSE);
 }
 
-static void	handle_collectible(t_game *game, int new_x, int new_y)
+//El margen puede ser muy ajustado o demasiado poco ajustado, habrá que ver
+static t_bool	can_move(t_game *game, double new_x, double new_y)
 {
-	if (game->map[new_y][new_x] == 'C')
-		game->collectibles--;
-	if (game->map[new_y][new_x] == 'E' && game->collectibles == 0)
-	{
-		printf("¡Has ganado!\n");
-		close_game(game);
-	}
+	const double	margin = 0.2;
+	int				map_x;
+	int				map_y;
+
+	map_x = (int)(new_x - margin);
+	map_y = (int)(new_y - margin);
+	if (is_wall(game, map_x, map_y))
+		return (FALSE);
+	map_x = (int)(new_x + margin);
+	map_y = (int)(new_y - margin);
+	if (is_wall(game, map_x, map_y))
+		return (FALSE);
+	map_x = (int)(new_x - margin);
+	map_y = (int)(new_y + margin);
+	if (is_wall(game, map_x, map_y))
+		return (FALSE);
+	map_x = (int)(new_x + margin);
+	map_y = (int)(new_y + margin);
+	if (is_wall(game, map_x, map_y))
+		return (FALSE);
+	return (TRUE);
 }
 
-void	move_player(t_game *game, int dx, int dy)
+void	move_player(t_game *game, double move_x, double move_y)
 {
-	int	new_x;
-	int	new_y;
+	double	new_x;
+	double	new_y;
 
-	new_x = game->player_x + dx;
-	new_y = game->player_y + dy;
-	if (game->map[new_y][new_x] == '1')
-		return ;
-	handle_collectible(game, new_x, new_y);
-	update_player_position(game, new_x, new_y);
-	if (dy == -1)
-		game->direction = 0;
-	else if (dy == 1)
-		game->direction = 1;
-	else if (dx == -1)
-		game->direction = 2;
-	else if (dx == 1)
-		game->direction = 3;
-	game->current_frame = (game->current_frame + 1) % 4;
-	game->moves++;
-	printf("Movimientos: %d\n", game->moves);
-	render_map(game);
+	new_x = game->player.pos.x + game->player.dir.x * move_x;
+	new_y = game->player.pos.y + game->player.dir.y * move_x;
+	new_x += game->player.plane.x * move_y;
+	new_y += game->player.plane.y * move_y;
+	if (can_move(game, new_x, game->player.pos.y))
+		game->player.pos.x = new_x;
+	if (can_move(game, game->player.pos.x, new_y))
+		game->player.pos.y = new_y;
+}
+
+void	rotate_player(t_game *game, double rot_speed)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = game->player.dir.x;
+	old_plane_x = game->player.plane.x;
+	game->player.dir.x = game->player.dir.x * cos(rot_speed) - \
+						game->player.dir.y * sin(rot_speed);
+	game->player.dir.y = old_dir_x * sin(rot_speed) + \
+						game->player.dir.y * cos(rot_speed);
+	game->player.plane.x = game->player.plane.x * cos(rot_speed) - \
+							game->player.plane.y * sin(rot_speed);
+	game->player.plane.y = old_plane_x * sin(rot_speed) + \
+							game->player.plane.y * cos(rot_speed);
+}
+
+//Hay que llamarlo desde el loop principal
+void	update_movement(t_game *game)
+{
+	if (game->keys.w)
+		move_player(game, MOVE_SPEED, 0);
+	if (game->keys.s)
+		move_player(game, -MOVE_SPEED, 0);
+	if (game->keys.a)
+		move_player(game, 0, -MOVE_SPEED);
+	if (game->keys.d)
+		move_player(game, 0, MOVE_SPEED);
+	if (game->keys.left)
+		rotate_player(game, -ROT_SPEED);
+	if (game->keys.right)
+		rotate_player(game, ROT_SPEED);
 }
