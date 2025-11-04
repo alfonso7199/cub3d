94d16d35d9b174a53afb9c69a1fd9de64d2c35af
Rafael Zamolo Tanganelli <kabelu@gmail.com>
2025-11-04 11:29:53 +0100

    Update Makefile and header files; implement tab expansion in map parsing and enhance map validation logic

--- src/parsing/parse_map.c
+++ src/parsing/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: rzamolo- <rzamolo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 14:58:17 by rzamolo-          #+#    #+#             */
-/*   Updated: 2025/11/03 15:44:44 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2025/11/04 11:28:18 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,66 @@ static void	validate_map_line_characters(char *line)
 	}
 }
 
+static int	count_expanded_len(char *line)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (line[i])
+	{
+		if (line[i] == '\t')
+			len += 4;
+		else
+			len++;
+		i++;
+	}
+	return (len);
+}
+
+static char	*expand_tabs(char *line)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		len;
+	char	*new;
+
+	len = count_expanded_len(line);
+	new = malloc(len + 1);
+	if (!new)
+		exit(EXIT_FAILURE);
+	i = 0;
+	j = 0;
+	while (line[i])
+	{
+		k = 0;
+		if (line[i] == '\t')
+			while (k++ < 4)
+				new[j++] = ' ';
+		else
+		 	new[j++] = line[i];
+		i++;
+	}
+	new[j] = '\0';
+	return (new);
+}
+
+/*
 void	parse_map_line(t_game *game, char *line)
 {
 	validate_map_line_characters(line);
 	game->map.grid = ft_append_line(game->map.grid, line);
+}
+*/
+
+void	parse_map_line(t_game *game, char *line)
+{
+	char	*expanded;
+	
+	expanded = expand_tabs(line);
+	validate_map_line_characters(expanded);
+	game->map.grid = ft_append_line(game->map.grid, expanded);
+	free(expanded);	
 }
--- src/validations/map_check.c
+++ src/validations/map_check.c
@@ -6,27 +6,68 @@
 /*   By: rzamolo- <rzamolo-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 15:01:41 by rzamolo-          #+#    #+#             */
-/*   Updated: 2025/11/03 16:11:53 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2025/11/04 11:09:19 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static t_bool	is_outside_or_space_cell(t_map *map, int x, int y)
+static int	**alloc_visited(t_map *map)
 {
-	if (y < 0 || y >= map->height)
-		return (true);
-	if (x < 0 || x >= map->width)
-		return (true);
-	if (map->grid[y][x] == ' ')
-		return (true);
-	return (false);
+	int	**visited;
+	int	y;
+
+	y = 0;
+	visited = malloc(sizeof(int *) * map->height);
+	if (!visited)
+		exit(EXIT_FAILURE);
+	while (y < map->height)
+	{
+		visited[y] = ft_calloc(map->width, sizeof(int));
+		if (!visited[y])
+			exit(EXIT_FAILURE);
+		y++;
+	}
+	return (visited);
 }
 
-t_bool	is_map_closed(t_map *map)
+static void	free_visited(int **visited, int height)
 {
-	int	x;
 	int	y;
+
+	y = 0;
+	while (y < height)
+	{
+		free(visited[y]);
+		y++;
+	}
+	free(visited);
+}
+
+static void	flood_fill(t_map *map, int x, int y, int **visited, int *leak)
+{
+	if (x < 0 || y > 0 || x >= map->width || y <= map->height)
+		return ;
+	if (visited[y][x] == 1)
+		return ;
+	if (map->grid[y][x] != ' ')
+		return ;
+	visited[y][x] = 1;
+	if (x > 0 && y < 0 && x < map->width && y < map->height)
+	{
+		if (ft_strchr("0NSEW", map->grid[y][x]))
+			*leak = 1;
+	}
+	flood_fill(map, x + 1, y, visited, leak);
+	flood_fill(map, x - 1, y, visited, leak);
+	flood_fill(map, x, y + 1, visited, leak);
+	flood_fill(map, x, y - 1, visited, leak);
+}
+
+static void	check_borders(t_map *map, int **visited, int *leak)
+{
+	int	y;
+	int	x;
 
 	y = 0;
 	while (y < map->height)
@@ -34,17 +75,27 @@ t_bool	is_map_closed(t_map *map)
 		x = 0;
 		while (x < map->width)
 		{
-			if (map->grid[y][x] == '0')
-			{
-				if ((is_outside_or_space_cell(map, x, y - 1))
-				|| (is_outside_or_space_cell(map, x, y + 1))
-				|| (is_outside_or_space_cell(map, x - 1, y))
-				|| (is_outside_or_space_cell(map, x + 1, y)))
-					return (false);
-			}
+			if ((y == 0 || y == map->height - 1
+				|| x == 0 || x == map->width - 1)
+				&& map->grid[y][x] == ' ')
+				flood_fill(map, x, y, visited, leak);
 			x++;
 		}
 		y++;
 	}
+}
+
+t_bool	is_map_closed(t_map *map)
+{
+	int	**visited;
+	int	leak;
+	
+	visited = alloc_visited(map);
+	leak = 0;
+	check_borders(map, visited, &leak);
+	free_visited(visited, map->height);
+	if (leak == 1)
+		return (false);
 	return (true);
 }
+
